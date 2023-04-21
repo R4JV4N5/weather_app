73c1df5016b21fb4d0d6ef5f6ef0a68d03d3f733
Raj Vanniyar <rajvenniyar65@gmail.com>
2023-04-21 22:37:43 +0530

    created flask project
    used bootstrap and flask

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request 
+import requests
+import json
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST": 
+        city = request.form['city']
+        api_key = "cad2fdbd5d1f0cc6688857d834a932b2"
+        weather_url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric')
+        print(weather_url)
+        weather_data = weather_url.json()
+        print(weather_data)
+        temp = round(weather_data['main']['temp']) 
+        humidity = weather_data['main']['humidity'] 
+        wind_speed = weather_data['wind']['speed']
+        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, city=city)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
