54b0e81740df3c3d3103195db8b87b6ecffba9e1
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2023-04-21 21:09:41 +0300

    hide your api key

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 import requests
 import json
-app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+app = Flask(__name__)
+api_key = open("APIKey.txt", "r").read().strip("\n")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST": 
-        city = request.form['city']
-        api_key = "cad2fdbd5d1f0cc6688857d834a932b2"
-        weather_url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric')
+    if request.method == "POST":
+        city = request.form["city"]
+        weather_url = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        )
         print(weather_url)
         weather_data = weather_url.json()
         print(weather_data)
-        temp = round(weather_data['main']['temp']) 
-        humidity = weather_data['main']['humidity'] 
-        wind_speed = weather_data['wind']['speed']
-        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, city=city)
+        temp = round(weather_data["main"]["temp"])
+        humidity = weather_data["main"]["humidity"]
+        wind_speed = weather_data["wind"]["speed"]
+        return render_template(
+            "result.html",
+            temp=temp,
+            humidity=humidity,
+            wind_speed=wind_speed,
+            city=city,
+        )
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
